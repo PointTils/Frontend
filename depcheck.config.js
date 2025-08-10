125e0c3e7fbd0a14955578f032bd2419bb111f72
Bianca Alves <biancadsalves@gmail.com>
2025-08-10 02:29:53 -0300

    added ignore dep

--- depcheck.config.js
+++ depcheck.config.js
@@ -1,26 +1,36 @@
 module.exports = {
-	ignoreDirs: ['node_modules', 'dist', 'build', 'android', 'ios', '.expo', '.output'],
+  ignoreDirs: [
+    'node_modules',
+    'dist',
+    'build',
+    'android',
+    'ios',
+    '.expo',
+    '.output',
+  ],
 
-	ignorePatterns: [
-		'jest.setup.ts',
-		'*.test.ts',
-		'*.test.tsx',
-		'*.spec.ts',
-		'*.spec.tsx',
-		'**/__tests__/**',
-		'babel.config.js',
-		'metro.config.js',
-		'jest.config.js',
-		'app.config.js',
-		'app.config.ts',
-	],
+  ignorePatterns: [
+    'jest.setup.ts',
+    '*.test.ts',
+    '*.test.tsx',
+    '*.spec.ts',
+    '*.spec.tsx',
+    '**/__tests__/**',
+    'babel.config.js',
+    'metro.config.js',
+    'jest.config.js',
+    'app.config.js',
+    'app.config.ts',
+  ],
 
-	specials: [
-		require('depcheck/special/eslint'),
-		require('depcheck/special/jest'),
-		require('depcheck/special/typescript'),
-		require('depcheck/special/webpack'),
-		require('depcheck/special/tslint'),
-		require('depcheck/special/babel'),
-	],
+  ignoreMatches: ['expo-splash-screen', 'expo-dev-client'],
+
+  specials: [
+    require('depcheck/special/eslint'),
+    require('depcheck/special/jest'),
+    require('depcheck/special/typescript'),
+    require('depcheck/special/webpack'),
+    require('depcheck/special/tslint'),
+    require('depcheck/special/babel'),
+  ],
 };
