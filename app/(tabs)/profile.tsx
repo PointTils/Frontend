956070728c340f2c4bae2f3ba3d253a27a1fd24a
Fernando Gazzana <fernando.gazzana@gmail.com>
2025-09-07 03:48:04 -0300

    feat: Profile edition page for every type of user, still missing some components, button functions and form verification.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,22 @@
 import { Text } from '@/src/components/ui/text';
 import { View } from '@/src/components/ui/view';
 import { Strings } from '@/src/constants/Strings';
+import { Button } from 'react-native';
+import { useRouter } from 'expo-router'
 
 export default function ProfileScreen() {
+  const router = useRouter()
+
+  // Frontend\app\(tabs)\(profile)\edit.tsx
   return (
     <View
       className="flex-1 items-center justify-center"
       accessibilityLabel={Strings.profile.title}
     >
-      <Text className="font-ifood-regular">{Strings.profile.title}</Text>
+      <Text>Edit</Text>
+      <Button title='Editar Perfil' onPress={() => router.push('/profile/edit')} />
+
     </View>
   );
 }
+
