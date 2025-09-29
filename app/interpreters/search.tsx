d194c0c0b3d2ed2cac9378e91de2b153def28c65
Vinicius Wittler <viniwittler@hotmail.com>
2025-09-29 19:51:18 -0300

    feat: add card to search screen

--- app/interpreters/search.tsx
+++ app/interpreters/search.tsx
@@ -1,4 +1,5 @@
 import Header from '@/src/components/Header';
+import { Card } from '@/src/components/ui/card';
 import { View } from '@/src/components/ui/view';
 import { router } from 'expo-router';
 import React from 'react';
@@ -6,14 +7,24 @@ import { KeyboardAvoidingView, Platform } from 'react-native';
 
 export default function SearchScreen() {
   return (
-    <View className="flex-1">
+    <View className="flex-1 w-full">
       <View className="mt-12 pb-2">
         <Header
-          title="AGENDAR"
+          title="PESQUISA"
           showBackButton={true}
           handleBack={() => router.back()}
         />
       </View>
+      <Card
+        variant="search"
+        photoUrl="https://img.freepik.com/free-photo/front-view-smiley-woman-with-earbuds_23-2148613052.jpg"
+        fullName="Nome Sobrenome"
+        specialty="Intérprete de Libras"
+        rating={4.5}
+        modality="Presencial/Online"
+        priceRange="R$ 100 - R$ 2.500"
+        location="Porto Alegre, Canoas & Gravataí"
+      />
 
       <KeyboardAvoidingView
         className="flex-1 px-6"
