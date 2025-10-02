98f3103341d93898119aa81d2291b2d6470762cb
Bianca Alves <biancadsalves@gmail.com>
2025-10-02 00:28:34 -0300

    add env

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,9 @@ export default function HomeScreen() {
       className="flex-1 items-center justify-center py-8"
       accessibilityLabel={Strings.home.tabBar}
     >
-      <Text className="font-ifood-regular">{Strings.home.tabBar}</Text>
+      <Text className="font-ifood-regular">
+        {process.env.EXPO_PUBLIC_API_URL}
+      </Text>
     </View>
   );
 }
