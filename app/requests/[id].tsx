e7636330bf2d0ae2a6c23181534214a2f58e7aa6
Bianca Alves <biancadsalves@gmail.com>
2025-10-14 18:39:08 -0300

    fix: use Strings and update InfoRow to pass icons

--- app/requests/[id].tsx
+++ app/requests/[id].tsx
@@ -1,15 +1,23 @@
+import HapticTab from '@/src/components/HapticTab';
 import Header from '@/src/components/Header';
 import { Avatar, AvatarImage } from '@/src/components/ui/avatar';
-import { Button, ButtonText, ButtonIcon } from '@/src/components/ui/button';
+import { Button, ButtonIcon } from '@/src/components/ui/button';
+import { InfoRow } from '@/src/components/ui/infoRow';
 import { Text } from '@/src/components/ui/text';
 import { View } from '@/src/components/ui/view';
 import { Strings } from '@/src/constants/Strings';
 import { useColors } from '@/src/hooks/useColors';
+import { getSafeAvatarUri } from '@/src/utils/helpers';
 import { router } from 'expo-router';
-import { SquarePen, CalendarDays, MapPin, Check, X } from 'lucide-react-native';
+import {
+  SquarePen,
+  CalendarDays,
+  MapPin,
+  XIcon,
+  CheckIcon,
+} from 'lucide-react-native';
 import React from 'react';
 import { ScrollView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Toast } from 'toastify-react-native';
 
 export default function RequestDetailsScreen() {
@@ -23,146 +31,128 @@ export default function RequestDetailsScreen() {
     // Show success toast
     Toast.show({
       type: 'success',
-      text1: 'Solicitação Aprovada',
-      text2: 'A solicitação foi aceita com sucesso!',
+      text1: Strings.requests.toast.acceptTitle,
+      text2: Strings.requests.toast.acceptDescription,
       position: 'top',
-      visibilityTime: 3000,
+      visibilityTime: 2000,
       autoHide: true,
       closeIconSize: 1,
     });
 
     // Go back to previous screen
-    router.back();
+    handleBack();
   };
 
   const handleReject = () => {
     // Show info toast
     Toast.show({
       type: 'info',
-      text1: 'Solicitação Recusada',
-      text2: 'A solicitação foi recusada.',
+      text1: Strings.requests.toast.rejectTitle,
+      text2: Strings.requests.toast.rejectDescription,
       position: 'top',
-      visibilityTime: 3000,
+      visibilityTime: 2000,
       autoHide: true,
       closeIconSize: 1,
     });
 
     // Go back to previous screen
-    router.back();
+    handleBack();
   };
 
   return (
-    <SafeAreaView className="flex-1 bg-background-0">
-      <Header
-        title={Strings.requests.request}
-        showBackButton={true}
-        handleBack={handleBack}
-      />
+    <View className="flex-1 justify-center">
+      <View className="mt-12 pb-2">
+        <Header
+          title={Strings.requests.request}
+          showBackButton={true}
+          handleBack={handleBack}
+        />
+      </View>
 
-      <ScrollView className="flex-1 px-6 ">
+      <ScrollView
+        className="w-full"
+        contentContainerClassName="grow px-6 pb-4"
+        showsVerticalScrollIndicator={false}
+      >
         {/* Requester information */}
         <View className="flex-row items-center justify-center mb-6 pt-4">
-          <Avatar size="2xl" borderRadius="full" className="mr-4">
+          <Avatar size="lg" borderRadius="full" className="mr-4 h-24 w-24">
             <AvatarImage
               source={{
-                uri: 'https://img.freepik.com/fotos-premium/beleza-e-feminilidade-linda-mulher-loira-com-longos-cabelos-loiros-sorrindo-retrato-natural_360074-56804.jpg',
+                uri: getSafeAvatarUri({
+                  remoteUrl: '',
+                }),
               }}
             />
           </Avatar>
 
           <View>
-            <Text className="text-typography-900 font-medium text-base mb-1">
+            <Text className="text-typography-900 font-ifood-medium mb-1">
               Nome Sobrenome
             </Text>
-            <Text className="text-typography-600 font-regular text-sm">
+            <Text className="text-typography-600 font-ifood-regular text-sm">
               XXX.XXX.XXX-XX
             </Text>
           </View>
         </View>
 
         {/* Appointment data */}
-        <View className="mb-8">
-          <Text className="text-typography-900 font-medium text-base mb-6">
-            {Strings.appointments.appointment}
+        <View className="mt-4">
+          <Text className="text-text-light font-ifood-medium text-lg mb-2">
+            {Strings.requests.details}
           </Text>
 
           {/* Separator line */}
           <View className="h-px bg-typography-200 mb-6" />
 
           {/* Description */}
-          <View className="flex-row items-start mb-6 gap-2">
-            <SquarePen size={16} color="#000000" className="mt-0.5" />
-            <View className="flex-1">
-              <Text className="text-typography-900 font-medium text-sm mb-2">
-                {Strings.common.fields.description}
-              </Text>
-              <Text className="text-typography-600 font-regular text-sm leading-5">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          <InfoRow
+            icon={<SquarePen size={16} color={colors.text} />}
+            label={Strings.common.fields.description}
+            value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris.
-              </Text>
-            </View>
-          </View>
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+          />
 
           {/* Date */}
-          <View className="flex-row items-start mb-6 gap-2">
-            <CalendarDays size={16} color="#000000" className="mt-0.5" />
-            <View className="flex-1">
-              <Text className="text-typography-900 font-medium text-sm mb-2">
-                {Strings.common.fields.date}
-              </Text>
-              <Text className="text-typography-600 font-regular text-sm">
-                20/08/2025 11:30 - 12:30
-              </Text>
-            </View>
-          </View>
+          <InfoRow
+            icon={<CalendarDays size={16} color={colors.text} />}
+            label={Strings.common.fields.date}
+            value="20/08/2025 11:30 - 12:30"
+          />
 
           {/* Location */}
-          <View className="flex-row items-start gap-2">
-            <MapPin size={16} color="#000000" className="mt-0.5" />
-            <View className="flex-1">
-              <Text className="text-typography-900 font-medium text-sm mb-2">
-                {Strings.common.fields.location}
-              </Text>
-              <Text className="text-typography-600 font-regular text-sm">
-                Av. Ipiranga 6681, Partenon - Porto Alegre/RS
-              </Text>
-            </View>
-          </View>
+          <InfoRow
+            icon={<MapPin size={16} color={colors.text} />}
+            label={Strings.common.fields.location}
+            value="Av. Ipiranga 6681, Partenon - Porto Alegre/RS"
+          />
         </View>
       </ScrollView>
 
-      {/* Action buttons */}
-      <View className="px-6 pb-6 pt-4 bg-background-0">
+      {/* Bottom buttons */}
+      <View className="px-6 pb-6 gap-4">
         <Button
-          size="lg"
-          action="primary"
+          size="md"
           onPress={handleAccept}
-          className="w-full mb-4"
-          style={{ backgroundColor: colors.primaryOrange }}
+          className="data-[active=true]:bg-primary-orange-press-light"
         >
-          <ButtonIcon as={Check} size={20} color="white" />
-          <ButtonText className="text-white font-medium">
-            {Strings.common.options.authorize}
-          </ButtonText>
+          <ButtonIcon as={CheckIcon} className="text-white" />
+          <Text className="font-ifood-regular text-text-dark">
+            {Strings.requests.accept}
+          </Text>
         </Button>
 
-        <Button
-          size="lg"
-          variant="link"
-          action="primary"
+        <HapticTab
           onPress={handleReject}
-          className="w-full"
+          className="flex-row justify-center gap-2 py-2"
         >
-          <ButtonIcon as={X} size={20} color={colors.primaryOrange} />
-          <ButtonText
-            className="font-medium"
-            style={{ color: colors.primaryOrange }}
-          >
-            {Strings.common.options.deny}
-          </ButtonText>
-        </Button>
+          <XIcon color={colors.primaryOrange} />
+          <Text className="font-ifood-regular text-primary-orange-light dark:text-primary-orange-dark">
+            {Strings.requests.reject}
+          </Text>
+        </HapticTab>
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
--- src/components/ui/infoRow/index.tsx
+++ src/components/ui/infoRow/index.tsx
@@ -1,25 +1,30 @@
 import { Text } from '@/src/components/ui/text';
 import React from 'react';
+import { View } from '../view';
 
 // esse é o componente da linha de informações da tela de perfil(apenas label e valor)
 
 type InfoRowProps = {
+  icon?: React.ReactNode;
   label: string;
   value?: string;
 };
 
-export function InfoRow({ label, value }: InfoRowProps) {
+export function InfoRow({ icon, label, value }: InfoRowProps) {
   if (!value) return null;
 
   return (
-    <>
-      <Text className="w-full pl-2 text-base font-ifood-medium text-left mb-1 text-primary-800">
-        {label}
-      </Text>
-      <Text className="w-full pl-2 text-base font-ifood-regular text-left mb-4 text-primary-800">
-        {value}
-      </Text>
-    </>
+    <View className={icon ? 'flex-row' : ''}>
+      {icon && <View>{icon}</View>}
+      <View className="flex-1 pl-2 ">
+        <Text className="font-ifood-medium text-left mb-1 text-text-light dark:text-text-dark">
+          {label}
+        </Text>
+        <Text className="font-ifood-regular text-left mb-4 text-text-light dark:text-text-dark">
+          {value}
+        </Text>
+      </View>
+    </View>
   );
 }
 
