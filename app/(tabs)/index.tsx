129bd01eb873babb6adb095b7088e4bb3af4e82f
Bianca Alves <biancadsalves@gmail.com>
2025-10-22 01:41:56 -0300

    style: fix spacings

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,7 +14,7 @@ import {
   AppointmentStatus,
 } from '@/src/types/api';
 import { renderApptItem } from '@/src/utils/helpers';
-import { CalendarDays } from 'lucide-react-native';
+import { CalendarDays, PackageSearchIcon } from 'lucide-react-native';
 import { useMemo } from 'react';
 import { ActivityIndicator, FlatList } from 'react-native';
 
@@ -63,7 +63,7 @@ export default function HomeScreen() {
   return (
     <View className="flex-1">
       <View className="pt-16">
-        <View className="flex-row px-2 pb-6 items-center gap-2">
+        <View className="flex-row px-4 pb-6 items-center gap-2">
           <DarkBlueLogo width={85} height={50} />
           <Text className="text-left text-2xl font-ifood-medium text-text max-w-[65%]">
             {welcomeMessage}
@@ -72,23 +72,16 @@ export default function HomeScreen() {
 
         <SearchFilterBar onData={() => {}} navigateOnSearch />
 
-        <View
-          className="h-px w-full mt-4"
-          style={{ backgroundColor: colors.fieldGray }}
-        />
-        <View className="flex-row items-center gap-3 pl-6 pt-4">
+        {/* Divider */}
+        <View className="w-full h-px bg-gray-200 mb-4 mt-6" />
+        <View className="flex-row items-center gap-3 px-4">
           <CalendarDays color={colors.primaryBlue} />
-          <Text
-            className="text-center font-ifood-medium"
-            style={{ color: colors.text }}
-          >
+          <Text className="text-text-light font-ifood-medium">
             {Strings.home.nextAppointments}
           </Text>
         </View>
-        <View
-          className="h-px w-full mt-4"
-          style={{ backgroundColor: colors.fieldGray }}
-        />
+        {/* Divider */}
+        <View className="w-full h-px bg-gray-200 mt-4" />
       </View>
 
       <View className="flex-1">
@@ -102,12 +95,10 @@ export default function HomeScreen() {
             ItemSeparatorComponent={() => <View className="h-3" />}
           />
         ) : (
-          <View className="flex-1 justify-center items-center py-8">
-            <Text
-              className="text-center font-ifood-regular"
-              style={{ color: colors.detailsGray }}
-            >
-              {Strings.common.noData}
+          <View className="flex-1 justify-center gap-y-4 items-center">
+            <PackageSearchIcon size={38} color={colors.detailsGray} />
+            <Text className="font-ifood-regular text-typography-400 text-md">
+              {Strings.common.noResults}
             </Text>
           </View>
         )}
