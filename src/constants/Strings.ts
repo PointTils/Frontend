652684737b0a84450d4eaa99e6770720ec663c7d
Vinicius Wittler <viniwittler@hotmail.com>
2025-10-29 18:42:37 -0300

    feat: new global strings to banner home

--- src/constants/Strings.ts
+++ src/constants/Strings.ts
@@ -129,6 +129,10 @@ export const Strings = {
     tabBar: 'Início',
     welcome: 'Olá, {User}!',
     nextAppointments: 'Próximos agendamentos',
+    banner:{
+      interpreter: 'Complete o seu perfil profissional',
+      person: 'Complete o seu perfil'
+    }
   },
 
   profile: {
