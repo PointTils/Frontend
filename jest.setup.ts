20c332174724c484c541ed912b729c0cb9449a57
Bianca Alves <biancadsalves@gmail.com>
2025-11-11 14:53:08 -0300

    fix(build): update jest setup - add report

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,13 +1,34 @@
 import type { ReactNode } from 'react';
 import '@testing-library/jest-native/extend-expect';
 
+// Deps mocks
 jest.mock('react-native-reanimated', () =>
   require('react-native-reanimated/mock'),
 );
+
 jest.mock('nativewind', () => {
   const actual = jest.requireActual('nativewind');
   return { ...actual, setColorScheme: () => {} };
 });
+
 jest.mock('@/src/components/ui/gluestack-ui-provider', () => ({
   GluestackUIProvider: ({ children }: { children: ReactNode }) => children,
+}));
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
+jest.mock('expo-document-picker', () => ({
+  getDocumentAsync: jest.fn(),
+}));
+
+jest.mock('expo-image-picker', () => ({
+  launchImageLibraryAsync: jest.fn(),
+  MediaTypeOptions: { Images: 'Images' },
+  PermissionStatus: { GRANTED: 'granted' },
+}));
+
+jest.mock('toastify-react-native', () => ({
+  Toast: { show: jest.fn() },
 }));
